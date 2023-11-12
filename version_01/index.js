fe2d8ddd790cf17fb9344e866cc20aeefae369d9
salinpaul valooran <salinpv@hotmail.com>
2023-11-12 21:05:27 +0530

    changing productImg src

--- version_01/index.js
+++ version_01/index.js
@@ -1,4 +1,6 @@
 let productImg = document.getElementById("productImg");
 let btn = document.getElementsByClassName("btn");
 
-btn[0].onclick = function () {};
+btn[0].onclick = function () {
+  productImg.src = "images/image1.png";
+};
